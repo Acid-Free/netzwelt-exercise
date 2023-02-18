ee876823cda771dff1da2ca8195dae1514e630cb
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-02-18 11:11:22 +0800

    Add account verification

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5500;
 
-let username;
-let password;
-
 app.set("view-engine", "ejs");
 
+// allows parsing of urlencoded payloads
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (request, response) => {
@@ -21,10 +19,28 @@ app.get("/account/login", (request, response) => {
   response.render("login.ejs");
 });
 
-app.post("/account/login", (request, response) => {
+app.post("/account/login", async (request, response) => {
   username = request.body.username;
   password = request.body.password;
+  response.send(await verifyAccount(username, password));
 });
+
+async function verifyAccount(username, password) {
+  // success: {"username":"foo","displayName":"Foo Bar Foo","roles":["basic-user"]}
+  // failure: {"message":"Invalid username or password."}
+  const response = await fetch(
+    "https://netzwelt-devtest.azurewebsites.net/Account/SignIn",
+    {
+      method: "post",
+      body: JSON.stringify({ username: username, password: password }),
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  const data = await response.json();
+
+  return data;
+}
 
 app.listen(port, () => {
   console.log("Listening...");
